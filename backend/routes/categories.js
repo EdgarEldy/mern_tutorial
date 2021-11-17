2bf8769e424a4c11c59a11ba53e1e0f69a2c91d2
Edgar Eldy <ed.eldy21@live.com>
2021-11-17 16:46:14 +0200

    Get all categories api

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -4,4 +4,10 @@ var router = express.Router();
 const db = require('../models');
 const Category = db.Category;
 
+// Get all categories api
+router.get('/categories', async (req, res, next) => {
+const categories = await Category.findAll();
+return res.json(categories);
+});
+
 module.exports = router;
