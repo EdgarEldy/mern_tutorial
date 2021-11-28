74da66656f75ebecfbaae8f48b2364fbd6beaf49
Edgar Eldy <ed.eldy21@live.com>
2021-11-28 23:10:06 +0200

    Add categories end point for creating a new category

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -6,8 +6,23 @@ const Category = db.Category;
 
 // Get all categories api
 router.get('/categories', async (req, res, next) => {
-const categories = await Category.findAll();
-return res.json(categories);
+    const categories = await Category.findAll();
+    return res.json(categories);
+});
+
+// Add a category end point
+router.post('/categories', async (req, res, next) => {
+
+    const category = {
+        category_name: req.body.category_name,
+    };
+
+    Category.create(category).then((data) => {
+        res.send(category.category_name = ' has been created successfully !');
+    })
+        .catch((err) => {
+            res.send('Error');
+        });
 });
 
 module.exports = router;
