9e2f6ae7d6f38ddae8319e1e69f14c299834e481
Edgar Eldy <ed.eldy21@live.com>
2022-04-17 21:42:18 +0300

    Add categories for testing

--- backend/seeders/20220417183831-category-seeder.js
+++ backend/seeders/20220417183831-category-seeder.js
@@ -1,24 +1,36 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+    up: async (queryInterface, Sequelize) => {
+        /**
+         * Add seed commands here.
+         *
+         * Example:
+         * await queryInterface.bulkInsert('People', [{
+         *   name: 'John Doe',
+         *   isBetaMember: false
+         * }], {});
+         */
+        return queryInterface.bulkInsert("categories", [
+            {
+                category_name: "Alcohols",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+            {
+                category_name: "Lemonades",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+        ]);
+    },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+    down: async (queryInterface, Sequelize) => {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+    }
 };
