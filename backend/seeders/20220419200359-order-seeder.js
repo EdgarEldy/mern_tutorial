a885af6c86b21cc5838c7b6115412da14021aafd
Edgar Eldy <ed.eldy21@live.com>
2022-04-19 23:06:14 +0300

    Add orders fake data for api testing

--- backend/seeders/20220419200359-order-seeder.js
+++ backend/seeders/20220419200359-order-seeder.js
@@ -1,24 +1,42 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+    up: async (queryInterface, Sequelize) => {
+        /**
+         * Add seed commands here.
+         *
+         * Example:
+         * await queryInterface.bulkInsert('People', [{
+         *   name: 'John Doe',
+         *   isBetaMember: false
+         * }], {});
+         */
+        return queryInterface.bulkInsert("orders", [
+            {
+                customer_id: 1,
+                product_id: 2,
+                qty: 2,
+                grand_total: 1600,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+            {
+                customer_id: 2,
+                product_id: 4,
+                qty: 2,
+                grand_total: 2000,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+        ]);
+    },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+    down: async (queryInterface, Sequelize) => {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+    }
 };
