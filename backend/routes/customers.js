da9c881c79b663862d5c5f74562211b30b8ed430
Edgar Eldy <ed.eldy21@live.com>
2022-04-25 13:11:08 +0200

    Update a customer

--- backend/routes/customers.js
+++ backend/routes/customers.js
@@ -44,4 +44,24 @@ router.get('/customers/:id', (req, res, next) => {
         });
 });
 
+// Update a customer
+router.put("/customers/edit/:id", function (req, res, next) {
+
+    const id = req.params.id;
+
+    Customer.update(req.body, {
+        where: {id: id}
+    }).then((data) => {
+        if (data === 1) {
+
+            res.send(data);
+        } else {
+            res.send("Customer has been updated successfully ")
+        }
+    }).catch((err) => {
+        res.send("Error")
+    });
+
+});
+
 module.exports = router;
