2a0de85a7d26b0a42150b8d65a958c3796e1c3f9
Edgar Eldy <ed.eldy21@live.com>
2022-04-27 20:30:50 +0300

    Initialize Order model

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -4,4 +4,7 @@ const router = express.Router();
 // Initialize database connection
 const db = require('../models');
 
+// Initialize Order model
+const Order = db.Order;
+
 module.exports = router;
