9bceec1d249d758a047093f4f2692fadc5c55769
Edgar Eldy <ed.eldy21@live.com>
2022-04-27 20:48:39 +0300

    Get orders related to customers and products

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -13,4 +13,22 @@ const Customer = db.Customer;
 // Initialize Product model
 const Product = db.Product;
 
+// Get orders related to customers and products
+router.get('/orders', async (req, res, next) => {
+    const orders = await Order.findAll({
+        include: [
+            {
+                model: Customer,
+                required: true
+            },
+            {
+                model: Product,
+                required: true
+            }
+        ]
+    });
+
+    return res.json(orders);
+});
+
 module.exports = router;
