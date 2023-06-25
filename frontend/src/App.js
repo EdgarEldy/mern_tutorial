87a4ac80726aae9f35b540d17e4ef73b4153bf85
EdgarEldy <ed.eldy21@live.com>
2023-06-25 20:41:42 +0200

    Register dashboard route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import DefaultLayout from "./components/layouts/DefaultLayout";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<DefaultLayout/>}>
+                    <Route index element={<Dashboard />}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
