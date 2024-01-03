31622f383eb5d5f8b0e6d60b0fa0615546e2e064
EdgarEldy <ed.eldy21@live.com>
2024-01-03 15:20:28 +0200

    Format files

--- backend/models/customer.js
+++ backend/models/customer.js
@@ -1,27 +1,27 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Customer extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Customer.init({
-    first_name: DataTypes.STRING,
-    last_name: DataTypes.STRING,
-    tel: DataTypes.STRING,
-    email: DataTypes.STRING,
-    address: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Customer',
-  });
-  return Customer;
+    class Customer extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    };
+    Customer.init({
+        first_name: DataTypes.STRING,
+        last_name: DataTypes.STRING,
+        tel: DataTypes.STRING,
+        email: DataTypes.STRING,
+        address: DataTypes.STRING
+    }, {
+        sequelize,
+        modelName: 'Customer',
+    });
+    return Customer;
 };
