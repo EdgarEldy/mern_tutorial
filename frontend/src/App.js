42a35d1b146bcf405fb4e0f78dd2e4ccaf07f8ca
EdgarEldy <ed.eldy21@live.com>
2024-01-20 22:59:32 +0200

    Add categories route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import DefaultLayout from "./components/layouts/DefaultLayout";
 import Dashboard from "./pages/Dashboard";
+import {CategoryList} from "./pages/categories/CategoryList";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<DefaultLayout/>}>
-                    <Route index element={<Dashboard />}/>
+                    <Route index element={<Dashboard/>}/>
+                </Route>
+                <Route path="/categories" element={<DefaultLayout/>}>
+                    <Route index element={<CategoryList/>}/>
                 </Route>
             </Routes>
         </BrowserRouter>
