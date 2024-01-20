8bbe710beb77ce43c7d3f040aaeb03254b3a7344
EdgarEldy <ed.eldy21@live.com>
2024-01-20 21:16:43 +0200

    Update default layout

--- frontend/src/components/layouts/DefaultLayout.jsx
+++ frontend/src/components/layouts/DefaultLayout.jsx
@@ -1,19 +1,21 @@
-import Nav from "../partials/Nav";
 import SideBar from "../partials/SideBar";
-import {Outlet} from "react-router";
+import { Outlet } from "react-router";
+import TopBar from "../partials/TopBar";
+import Footer from "../partials/Footer";
 
 function DefaultLayout() {
-    return (
-        <div>
-            <Nav/>
-            <SideBar/>
-            <div className="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
-                <div className="row">
-                    <Outlet/>
-                </div>
-            </div>
+  return (
+    <div id="wrapper">
+      <SideBar />
+      <div id="content-wrapper" className="d-flex flex-column">
+        <div id="content">
+          <TopBar />
+          <Outlet />
         </div>
-    );
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default DefaultLayout;
