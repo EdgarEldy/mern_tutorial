11086ae34fa0eb4da83dcc54f1ae7f382566e802
EdgarEldy <ed.eldy21@live.com>
2024-01-20 21:17:43 +0200

    Update Sidebar component

--- frontend/src/components/partials/SideBar.jsx
+++ frontend/src/components/partials/SideBar.jsx
@@ -1,86 +1,109 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function SideBar() {
-    return (
-        <div>
-            <div id="sidebar-collapse" className="col-sm-3 col-lg-2 sidebar">
-                <ul className="nav menu">
-                    <li className="active"><Link to="/">
-                        <svg className="glyph stroked dashboard-dial">
-                            <use xlinkHref="#stroked-dashboard-dial"></use>
-                        </svg>
-                        Dashboard</Link></li>
-                    <li><a href>
-                        <svg className="glyph stroked calendar">
-                            <use xlinkHref="#stroked-calendar"></use>
-                        </svg>
-                        Orders</a></li>
-                    <li><a href>
-                        <svg className="glyph stroked clock">
-                            <use xlinkHref="#stroked-clock"></use>
-                        </svg>
-                        Customers</a></li>
-                    <li className="parent">
-                        <a href><span data-toggle="collapse" href="#sub-item-1"><svg
-                            className="glyph stroked chevron-down">
-                                    <use xlinkHref="#stroked-chevron-down"/>
-                                </svg></span> Products
-                        </a>
-                        <ul className="children collapse" id="sub-item-1">
-                            <li><a className href>
-                                <svg className="glyph stroked chevron-right">
-                                    <use xlinkHref="#stroked-chevron-right">
-                                    </use>
-                                </svg>
-                                All products
-                            </a>
-                            </li>
-                            <li><a className href>
-                                <svg className="glyph stroked chevron-right">
-                                    <use xlinkHref="#stroked-chevron-right">
-                                    </use>
-                                </svg>
-                                Categories
-                            </a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li className="parent">
-                        <a href><span data-toggle="collapse" href="#sub-item-2"><svg
-                            className="glyph stroked chevron-down">
-                                    <use xlinkHref="#stroked-chevron-down"/>
-                                </svg></span> Access control
-                        </a>
-                        <ul className="children collapse" id="sub-item-2">
-                            <li><a className href>
-                                <svg className="glyph stroked chevron-right">
-                                    <use xlinkHref="#stroked-chevron-right">
-                                    </use>
-                                </svg>
-                                Roles
-                            </a>
-                            </li>
-                            <li><a className href>
-                                <svg className="glyph stroked chevron-right">
-                                    <use xlinkHref="#stroked-chevron-right">
-                                    </use>
-                                </svg>
-                                Users
-                            </a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li><a href>
-                        <svg className="glyph stroked male-user">
-                            <use xlinkHref="#stroked-cancel"/>
-                        </svg>
-                        Logout</a>
-                    </li>
-                </ul>
+  return (
+    <div>
+      {/* Sidebar */}
+      <ul
+        className="navbar-nav bg-gray-200 sidebar sidebar-dark accordion"
+        id="accordionSidebar"
+      >
+        {/* Sidebar - Brand */}
+        <a
+          className="sidebar-brand d-flex align-items-center justify-content-center"
+          href="index.html"
+        >
+          <div className="sidebar-brand-icon rotate-n-15"></div>
+          <div className="sidebar-brand-text mx-3">REACT.JS TUTORIAL</div>
+        </a>
+        {/* Nav Item - Dashboard */}
+        <li className="nav-item">
+          <Link to="/" className="nav-link">
+            <i className="fas fa-fw fa-home" />
+            <span>Accueil</span>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href>
+            <i className="fas fa-shopping-cart" />
+            <span>Orders</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href>
+            <i className="fas fa-fw fa-users" />
+            <span>Customers</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href
+            className="nav-link collapsed"
+            data-toggle="collapse"
+            data-target="#collapseEvents"
+            aria-expanded="true"
+            aria-controls="collapseEvents"
+          >
+            <i className="fas fa-fw fa-shopping-bag" />
+            <span>Products</span>
+          </a>
+          <div
+            id="collapseEvents"
+            className="collapse"
+            aria-labelledby="headingEvents"
+            data-parent="#accordionSidebar"
+          >
+            <div className="bg-white py-2 collapse-inner rounded">
+              <a className="collapse-item" href>
+                All
+              </a>
+              <a className="collapse-item" href>
+                Categories
+              </a>
             </div>
+          </div>
+        </li>
+        <li className="nav-item">
+          <a
+            href
+            className="nav-link collapsed"
+            data-toggle="collapse"
+            data-target="#collapseSettings"
+            aria-expanded="true"
+            aria-controls="collapseSettings"
+          >
+            <i className="fas fa-fw fa-cog" />
+            <span>Access control</span>
+          </a>
+          <div
+            id="collapseSettings"
+            className="collapse"
+            aria-labelledby="headingSettings"
+            data-parent="#accordionSidebar"
+          >
+            <div className="bg-white py-2 collapse-inner rounded">
+              <a className="collapse-item" href>
+                Roles
+              </a>
+              <a className="collapse-item" href>
+                Users
+              </a>
+            </div>
+          </div>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href>
+            <i className="fas fa-fw fa-sign-out-alt" />
+            <span>Logout</span>
+          </a>
+        </li>
+        {/* Sidebar Toggler (Sidebar) */}
+        <div className="text-center d-none d-md-inline">
+          <button className="rounded-circle border-0" id="sidebarToggle" />
         </div>
-
-    )
+      </ul>
+    </div>
+  );
 }
 
 export default SideBar;
