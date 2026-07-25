45d38da3163090373eec591ecda762ff780969d0
EdgarEldy <ed.eldy21@live.com>
2026-07-25 16:31:48 +0400

    refactor(customers): update index.js - remove CustomerFormPage export
    CustomerFormPage deleted. Public API: CustomerListPage and CustomerSelect.

--- frontend/src/features/customers/index.js
+++ frontend/src/features/customers/index.js
@@ -1,3 +1,2 @@
 export { default as CustomerListPage } from './pages/CustomerListPage';
-export { default as CustomerFormPage } from './pages/CustomerFormPage';
 export { default as CustomerSelect } from './components/CustomerSelect';
