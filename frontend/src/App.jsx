3827b270f9ed0d7847321913beffa3b1029ebc0f
EdgarEldy <ed.eldy21@live.com>
2026-07-25 16:22:50 +0400

    refactor(categories): update App.jsx - remove /categories/new and edit routes
    The two form routes are no longer needed since create/edit now happen
    inside a modal on /categories. Only the list route remains.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import DefaultLayout from './components/layouts/DefaultLayout';
 import Dashboard from './pages/Dashboard';
-import { CategoryListPage, CategoryFormPage } from './features/categories';
+import { CategoryListPage } from './features/categories';
 import { ProductListPage, ProductFormPage } from './features/products';
 import { CustomerListPage, CustomerFormPage } from './features/customers';
 import { OrderListPage, OrderFormPage } from './features/orders';
@@ -13,8 +13,6 @@ function App() {
         <Route path="/" element={<DefaultLayout />}>
           <Route index element={<Dashboard />} />
           <Route path="categories" element={<CategoryListPage />} />
-          <Route path="categories/new" element={<CategoryFormPage />} />
-          <Route path="categories/:id/edit" element={<CategoryFormPage />} />
           <Route path="products" element={<ProductListPage />} />
           <Route path="products/new" element={<ProductFormPage />} />
           <Route path="products/:id/edit" element={<ProductFormPage />} />
